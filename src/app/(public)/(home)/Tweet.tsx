8ed53740a7dc2380b1006662b179beeb641a2e91
Ernest-Projects <ernestpopovic5621@gmail.com>
2026-02-04 14:23:41 +0200

    Changing border color

--- src/app/(public)/(home)/Tweet.tsx
+++ src/app/(public)/(home)/Tweet.tsx
@@ -11,7 +11,7 @@ interface Props {
 export const Tweet = ({ tweet }: Props) => {
   return (
     <>
-      <div className="w-full h-fit border-gray-500/20  border rounded-md gap-4 px-4 py-2 text-white ">
+      <div className="w-full h-fit border-neutral-500/20  border rounded-md gap-4 px-4 py-2 text-white ">
         <div className="rounded-full items-center w-8 h-8 flex flex-row">
           
           <Image src="/X_icon_2.svg" width = {24} height={24} alt="x-logo" />
