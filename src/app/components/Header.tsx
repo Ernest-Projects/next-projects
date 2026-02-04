1ca23539ba3f3331f0676894d42c9aa7226e44e1
Ernest-Projects <ernestpopovic5621@gmail.com>
2026-02-04 14:29:45 +0200

    refactor(Header.tsx): move nav block to Main.tsx

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,25 +1,24 @@
-import Link from "next/link"
-import Image from "next/image"
-
-
+import Link from "next/link";
+import Image from "next/image";
+import { Menu } from "./Menu";
 
 export const Header = () => {
-    return (<>
-        <header className="border-b border-white/10 px-6 py-4 flex items-center justify-between bg-black">
+  return (
+    <>
+      <header className="border-b border-white/10 px-6 py-4 flex items-center justify-between bg-black">
+        <Link href={"/"} className="flex items-center gap-3">
+          <Image
+            src={"/X_icon_2.svg"}
+            alt={"X icon"}
+            width={28}
+            height={28}
+            priority
+          />
+        </Link>
 
-            <Link href={"/"}
-            className="flex items-center gap-3"
-            >
-                <Image src={"/X_icon_2.svg"} alt =  {"X icon"} width={28} height={28} priority/>
-            </Link>
-
-
-        <nav className="flex gap-6  text-white/80">
-            <Link href="/">Home</Link>
-            <Link href="/">Explore</Link>
-            <Link href="/">Profile</Link>
-
-         </nav>
-        </header>
-    </>)
-}
+        <Menu></Menu>
+        
+      </header>
+    </>
+  );
+};
