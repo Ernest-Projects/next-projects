80feac646e3a6614f174d79c6a3224a4a7b4674b
Ernest-Projects <ernestpopovic5621@gmail.com>
2026-02-13 20:47:24 +0200

    strict mode - on, change env variable name, added domen for fake api

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  reactStrictMode: true,
+  images: {
+    domains: ['fakestoreapi.com']
+  },
+  env: {
+    FAKEAPI_URL: process.env.NEXT_API_URL
+  }
 };
 
 export default nextConfig;
