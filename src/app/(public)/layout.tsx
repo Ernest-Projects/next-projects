daf52020ffa2fff9e1bffcb4ddaccefc6e40f154
Ernest-Projects <ernestpopovic5621@gmail.com>
2026-02-13 20:44:23 +0200

    fix

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,12 +1,13 @@
 import type { PropsWithChildren } from "react";
 import { Header } from "../components/Header";
 
-
-
-export default function Layout({children}: PropsWithChildren<unknown>) {
-    return <main className="min-h-screen w-full h-full">
-        <Header/>
+export default function Layout({ children }: PropsWithChildren<unknown>) {
+  return (
+    <main className="min-h-screen w-full h-full">
+      <Header />
       <div className="w-full max-w-xl flex justify-center px-4 py-8">
-        {children}</div>
-      </main>
+        {children}
+      </div>
+    </main>
+  );
 }
