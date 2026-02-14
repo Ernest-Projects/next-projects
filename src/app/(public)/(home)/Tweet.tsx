c6cbadf12c08dca2ddef880c1cdb17ca1adb5b4d
Ernest-Projects <ernestpopovic5621@gmail.com>
2026-02-14 10:46:51 +0200

    fix

--- src/app/(public)/(home)/Tweet.tsx
+++ src/app/(public)/(home)/Tweet.tsx
@@ -13,12 +13,12 @@ export const Tweet = ({ tweet }: Props) => {
     <>
       <div className="w-full h-fit border-neutral-500/20  border rounded-md gap-4 px-4 py-2 text-white ">
         <div className="rounded-full items-center w-8 h-8 flex flex-row">
-          
-          <Image src="/X_icon_2.svg" width = {24} height={24} alt="x-logo" />
+          <Image src="/X_icon_2.svg" width={24} height={24} alt="x-logo" />
 
-          <Link href={PAGES.PROFILE(tweet.author)} className="ml-2 text-sm">@{tweet.author}</Link>
+          <Link href={PAGES.PROFILE(tweet.author)} className="ml-2 text-sm">
+            @{tweet.author}
+          </Link>
         </div>
-
         <p className="ml-2 text-sm">{tweet.content}</p>
       </div>
     </>
